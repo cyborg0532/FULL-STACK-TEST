b95491c292bdce204ac64b9a0bccd8f790966182
cyborg0532 <sashmitdarbari@gmail.com>
2026-09-25 12:52:18 +0530

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const profileRoutes = require('./src/routes/profile.routes');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/profiles', profileRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server safely running without Mongo on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
